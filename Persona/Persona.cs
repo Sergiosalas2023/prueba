aec5524ef8dd3a4d743f19c3722ae7272386ebab
APRENDIZ <APRENDIZ@BOGCPCDMF030302.FormacionSena.RED>
2024-03-09 11:09:47 -0500

    first commit

---
+++ Persona/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona
+{
+    public class Persona
+    {
+        
+        public string Nombre { get; set; }
+        public DateTime Fecha_Nacimiento { get; set; }
+        public string Sexo { get; set; }
+        public string Color_Piel { get; set; }
+        public string Color_Ojos { get; set; }
+        public double Estatura { get; set; }
+        public double Peso { get; set; }
+        public int Edad { get; set; } 
+    }
+}
